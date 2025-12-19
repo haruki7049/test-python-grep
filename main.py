e302d480b87d2c9eaf1ddfd68dd4472e94ea6df8
haruki7049 <tontonkirikiri@gmail.com>
2025-12-19 19:39:16 +0900

    Update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import termcolor
 
 class Grep:
     @staticmethod
-    def grep_from_str(text: str, target: str):
+    def grep_from_str(text: str, target: str, is_colored_output: bool):
         lines: list[str] = text.splitlines()
 
         for line in lines:
@@ -13,13 +13,17 @@ class Grep:
                 # To print the line with colored target term, Split line by target term
                 terms: list[str] = line.partition(target)
 
-                for term in terms:
-                    if term is target:
-                        termcolor.cprint(term, 'red', end='')
-                    else:
-                        print(term, end='')
+                if is_colored_output:
+                    for term in terms:
+                        if term is target:
+                            termcolor.cprint(term, 'red', end='')
+                        else:
+                            print(term, end='')
 
-                print()
+                    print()
+
+                else:
+                    print(line)
 
 
 class FileReader:
@@ -32,9 +36,10 @@ class FileReader:
 @click.command()
 @click.argument("path", type=str)
 @click.argument("target", type=str)
-def main(path: str, target: str):
+@click.option("--colored-output", is_flag=True)
+def main(path: str, target: str, colored_output: bool):
     context = FileReader.read_path(path)
-    Grep.grep_from_str(context, target)
+    Grep.grep_from_str(context, target, color)
 
 
 if __name__ == "__main__":
