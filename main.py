096a40b6889f6dc2c1dfdc297a41ba246fff1ffc
haruki7049 <tontonkirikiri@gmail.com>
2025-12-19 18:58:16 +0900

    Update

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
-def main():
-    print("Hello from python-grep!")
+import click
+import os
+
+
+class Grep:
+    @staticmethod
+    def grep_from_str(text: str, target: str):
+        lines = text.splitlines()
+        for line in lines:
+            if target in line:
+                print(line)
+
+
+class FileReader:
+    @staticmethod
+    def read_path(path: str) -> str:
+        with open(path, "r") as f:
+            return f.read();
+
+
+@click.command()
+@click.argument("path", type=str)
+@click.argument("target", type=str)
+def main(path: str, target: str):
+    context = FileReader.read_path(path)
+    Grep.grep_from_str(context, target)
 
 
 if __name__ == "__main__":
